ce694f65b57fd91eb9ccc880c96561c399520c8e
imanzuhrah <iman.musliamdev@gmail.com>
2025-07-12 15:54:56 -0300

    Update 1

---
+++ model/retangulo.py
@@ -0,0 +1,15 @@
+# Classe de objetos do tipo retângulo
+class retangulo:
+    
+    # Método construtor de objetos.
+    def __init__(self, largura: float, altura: float):
+        self.largura = largura
+        self.altura = altura
+    
+    # Método calcular a área do retângulo.
+    def calcular_area(self) -> float:
+        return self.largura * self.altura
+
+    # Método calcular o perímetro do retângulo.
+    def calcular_area(self) -> float:
+        return 2 * (self.largura + self.altura)
